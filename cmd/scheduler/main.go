002b6195114af73060eaad9725f7c88269d448fc
Camilo Acuña <camilo.aku@gmail.com>
2017-10-07 17:38:12 -0300

    add changes to use yml config file

--- cmd/scheduler/main.go
+++ cmd/scheduler/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net"
+	"strconv"
 	"time"
 
 	"github.com/WiseGrowth/pigeon/pigeon/proto"
@@ -12,21 +14,58 @@ import (
 	"github.com/WiseGrowth/pigeon/scheduler"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	yaml "gopkg.in/yaml.v2"
 )
 
+// Config define all env values used for service execution
+type Config struct {
+	Port             string `yaml:"port"`
+	Host             string `yaml:"host"`
+	DbFile           string `yaml:"db_file"`
+	RedisURL         string `yaml:"redis_url"`
+	RedisIdleTimeout string `yaml:"redis_idle_timeout"`
+	RedisDatabase    string `yaml:"redis_database"`
+	RedisMaxIdle     string `yaml:"redis_max_idle"`
+}
+
 func main() {
-	port := flag.Int("port", 5050, "port of the service")
-	host := flag.String("host", "", "host of the service")
-	dbfile := flag.String("db", "massages.db", "file to store messages")
+	var config Config
 
-	redisURL := flag.String("redis_url", "redis://127.0.0.1:6379", "URL of the redis server.")
-	redisIdleTimeout := flag.Duration("redis_idle_timeout", 5*time.Second, "Timeout for redis idle connections.")
-	redisDatabase := flag.Int("redis_db", 1, "Redis database to use")
-	redisMaxIdle := flag.Int("redis_max_idle", 10, "Maximum number of idle connections in the pool")
-
+	configPath := flag.String("path", "config.yml", "path with config file")
 	flag.Parse()
 
-	addr := fmt.Sprintf("%s:%d", *host, *port)
+	file, err := ioutil.ReadFile(*configPath)
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	err = yaml.Unmarshal(file, &config)
+	if err != nil {
+		panic(err)
+	}
+
+	port, err := strconv.Atoi(config.Port)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	redisIdleTimeout, err := strconv.Atoi(config.RedisIdleTimeout)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	redisDatabase, err := strconv.Atoi(config.RedisDatabase)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	redisMaxIdle, err := strconv.Atoi(config.RedisMaxIdle)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	addr := fmt.Sprintf("%s:%d", config.Host, port)
+
 	lis, err := net.Listen("tcp", addr)
 	if err != nil {
 		log.Fatal(err)
@@ -34,14 +73,14 @@ func main() {
 
 	s := grpc.NewServer()
 
-	log.Printf("Starting server at %s redis_url: %s redis_db: %d database: %s\n", addr, *redisURL, *redisDatabase, *dbfile)
+	log.Printf("Starting server at %s redis_url: %s redis_db: %d database: %s\n", addr, config.RedisURL, redisDatabase, config.DbFile)
 
 	proto.RegisterSchedulerServiceServer(s, schedulersvc.New(scheduler.StorageConfig{
-		BoltDatabase:     *dbfile,
-		RedisURL:         *redisURL,
-		RedisIdleTimeout: *redisIdleTimeout,
-		RedisDatabase:    *redisDatabase,
-		RedisMaxIdle:     *redisMaxIdle,
+		BoltDatabase:     config.DbFile,
+		RedisURL:         config.RedisURL,
+		RedisIdleTimeout: time.Duration(redisIdleTimeout) * time.Second,
+		RedisDatabase:    redisDatabase,
+		RedisMaxIdle:     redisMaxIdle,
 	}))
 	reflection.Register(s)
 
