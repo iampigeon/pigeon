1fe112ce454f8fde2f05f5b7688f9dfb60741def
Camilo Acuña <camilo.aku@gmail.com>
2018-10-12 07:28:29 -0300

    move db code to datastorage refactor in Put method add update status for lifecycle message

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -4,12 +4,10 @@ import (
 	"context"
 	"log"
 	"net"
-	"os"
 	"time"
 
-	"github.com/boltdb/bolt"
-	"github.com/gogo/protobuf/proto"
 	"github.com/iampigeon/pigeon"
+	"github.com/iampigeon/pigeon/db"
 	pb "github.com/iampigeon/pigeon/proto"
 	"github.com/oklog/ulid"
 	"github.com/pkg/errors"
@@ -20,37 +18,25 @@ import (
 
 // StorageConfig is a struct that will be deleted.
 type StorageConfig struct {
-	BoltDatabase     string        // File to use as bolt database.
+	// BoltDatabase     string        // File to use as bolt database.
 	RedisURL         string        // URL of the redis server
 	RedisLog         bool          // log database commands
 	RedisMaxIdle     int           // maximum number of idle connections in the pool
 	RedisDatabase    int           // redis database to use
 	RedisIdleTimeout time.Duration // timeout for idle connections
+
+	MessageStore *db.MessageStore
 }
 
 // New builds a new pigeon.Store backed by bolt DB.
 //
 // In case of any error it panics.
 func New(config StorageConfig) pigeon.SchedulerService {
-	db, err := bolt.Open(config.BoltDatabase, os.ModePerm, nil)
-	if err != nil {
-		log.Println("4444")
-		panic(err)
-	}
-
-	err = db.Update(func(tx *bolt.Tx) error {
-		_, berr := tx.CreateBucketIfNotExists(msgBucket)
-		return berr
-	})
-	if err != nil {
-		log.Println("555")
-		panic(err)
-	}
-
 	s := &service{
-		db:  db,
 		pq:  newPriorityQueue(config),
 		idc: make(chan ulid.ULID),
+
+		ms: config.MessageStore,
 	}
 
 	go s.run()
@@ -61,13 +47,15 @@ func New(config StorageConfig) pigeon.SchedulerService {
 var msgBucket = []byte("messages")
 
 type service struct {
-	db *bolt.DB
+	// db *bolt.DB
 	pq *priorityQueue
 
 	idc chan ulid.ULID
+
+	ms *db.MessageStore
 }
 
-func (s *service) Put(id ulid.ULID, content []byte, endpoint pigeon.NetAddr) error {
+func (s *service) Put(id ulid.ULID, content []byte, endpoint pigeon.NetAddr, status pigeon.MessageStatus, subjectID string) error {
 	// TODO(ja): use secure connections
 
 	host, port, err := net.SplitHostPort(string(endpoint))
@@ -87,32 +75,36 @@ func (s *service) Put(id ulid.ULID, content []byte, endpoint pigeon.NetAddr) err
 	client := pb.NewBackendServiceClient(conn)
 	resp, err := client.Approve(context.Background(), &pb.ApproveRequest{Content: content})
 	if err != nil {
+		// update status to crashed-approve
+		e := s.ms.UpdateStatus(id, pigeon.StatusCrashedApprove)
+		if e != nil {
+			return e
+		}
+
 		return err
 	}
 	if !resp.Valid {
+		// update status to failed-approve
+		e := s.ms.UpdateStatus(id, pigeon.StatusFailedApprove)
+		if e != nil {
+			return e
+		}
+
 		if resp.Error != nil {
 			return errors.Errorf("invalid message, %s", resp.Error.Message)
 		}
 		return errors.New("invalid message")
 	}
 
-	err = s.db.Update(func(tx *bolt.Tx) error {
-		b := tx.Bucket(msgBucket)
+	m := pigeon.Message{
+		ID:        id,
+		Content:   content,
+		Endpoint:  endpoint,
+		Status:    status,
+		SubjectID: subjectID,
+	}
 
-		k, merr := id.MarshalBinary()
-		if merr != nil {
-			return merr
-		}
-		v, jerr := proto.Marshal(&pb.Message{
-			Id:       id.String(),
-			Content:  content,
-			Endpoint: string(endpoint),
-		})
-		if jerr != nil {
-			return jerr
-		}
-		return b.Put(k, v)
-	})
+	err = s.ms.AddMessage(m)
 	if err != nil {
 		return err
 	}
@@ -123,49 +115,22 @@ func (s *service) Put(id ulid.ULID, content []byte, endpoint pigeon.NetAddr) err
 }
 
 func (s *service) Get(id ulid.ULID) (*pigeon.Message, error) {
-	var msg pb.Message
-	err := s.db.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket(msgBucket)
-		k, err := id.MarshalBinary()
-		if err != nil {
-			return err
-		}
-		v := b.Get(k)
-		if err := proto.Unmarshal(v, &msg); err != nil {
-			return err
-		}
-		return nil
-	})
+	msg, err := s.ms.GetMessage(id)
 	if err != nil {
 		return nil, err
 	}
 
-	return &pigeon.Message{
-		ID:       id,
-		Content:  msg.Content,
-		Endpoint: pigeon.NetAddr(msg.Endpoint),
-	}, nil
+	return msg, nil
 }
 
+// TODO(ca): change Update protobuf params method
 func (s *service) Update(id ulid.ULID, content []byte) error {
-	var msg pb.Message
-	return s.db.Update(func(tx *bolt.Tx) error {
-		b := tx.Bucket(msgBucket)
-		k, err := id.MarshalBinary()
-		if err != nil {
-			return err
-		}
-		v := b.Get(k)
-		if err = proto.Unmarshal(v, &msg); err != nil {
-			return err
-		}
-		msg.Content = content
-		v, err = proto.Marshal(&msg)
-		if err != nil {
-			return err
-		}
-		return b.Put(k, v)
-	})
+	err := s.ms.UpdateContent(id, content)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // Run in its goroutine
@@ -236,10 +201,34 @@ func (s *service) send(id ulid.ULID) {
 	resp, err := client.Deliver(context.Background(), &pb.DeliverRequest{Content: msg.Content})
 	if err != nil {
 		log.Printf("Error: could not deliver message %s, %v", msg.ID, err)
+
+		// update status to crashed-deliver
+		e := s.ms.UpdateStatus(id, pigeon.StatusCrashedDeliver)
+		if e != nil {
+			log.Printf("Error: could not update message status %s, %v", msg.ID, err)
+			return
+		}
+
 		return
 	}
 	if resp.Error != nil {
 		log.Printf("Error: failed to deliver message %s, %v", msg.ID, resp.Error.Message)
+
+		// update status to failed-deliver
+		e := s.ms.UpdateStatus(id, pigeon.StatusFailedDeliver)
+		if e != nil {
+			log.Printf("Error: could not update message status %s, %v", msg.ID, err)
+			return
+		}
+
 		return
 	}
+
+	e := s.ms.UpdateStatus(id, pigeon.StatusSent)
+	if e != nil {
+		log.Printf("Error: could not update message status %s, %v", msg.ID, err)
+		return
+	}
+
+	// TODO(ca): send Put Message with 'callback_post_url' message to pigeon-http
 }
