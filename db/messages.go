f67f5ee3cb2a85dd0e62889aaf77015b82edf236
mtavano <chelotavano.udp@gmail.com>
2018-10-20 11:33:08 -0300

    Introduce arangodb collection inside of MessageStore

--- db/messages.go
+++ db/messages.go
@@ -1,22 +1,82 @@
 package db
 
 import (
+	"context"
+
 	"github.com/boltdb/bolt"
 	"github.com/gogo/protobuf/proto"
 	"github.com/iampigeon/pigeon"
 	"github.com/oklog/ulid"
 
+	arango "github.com/arangodb/go-driver"
 	pb "github.com/iampigeon/pigeon/proto"
+)
+
+const (
+	msgCollection = "message_collection"
 )
 
 // MessageStore ...
 type MessageStore struct {
-	*Datastore
+	Dst        *Datastore
+	Collection arango.Collection
+}
+
+// NewMessageStore ...
+func NewMessageStore(dst *Datastore) (*MessageStore, error) {
+	found, err := dst.PigeonDB.CollectionExists(*dst.Context, msgCollection)
+	if err != nil {
+		return nil, err
+	}
+
+	var col arango.Collection
+
+	if !found {
+		opt := new(arango.CreateCollectionOptions)
+		col, err = dst.PigeonDB.CreateCollection(*dst.Context, msgCollection, opt)
+		if err != nil {
+			return nil, err
+		}
+
+		return &MessageStore{
+			Dst:        dst,
+			Collection: col,
+		}, nil
+	}
+
+	col, err = dst.PigeonDB.Collection(*dst.Context, msgCollection)
+	if err != nil {
+		return nil, err
+	}
+
+	return &MessageStore{
+		Dst:        dst,
+		Collection: col,
+	}, nil
+}
+
+// StoreMessage ...
+func (ss *MessageStore) StoreMessage(m *pigeon.Message) error {
+	ctx := context.Background()
+	msg := map[string]interface{}{
+		"id":         m.ID.String(),
+		"content":    m.Content,
+		"endpoint":   string(m.Endpoint),
+		"status":     string(m.Status),
+		"subject_id": string(m.SubjectID),
+	}
+
+	_, err := ss.Collection.CreateDocument(ctx, msg)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // AddMessage ...
 func (ss *MessageStore) AddMessage(m pigeon.Message) error {
-	err := ss.DB.Update(func(tx *bolt.Tx) error {
+	err := ss.Dst.DB.Update(func(tx *bolt.Tx) error {
 		b := tx.Bucket(msgBucket)
 
 		k, merr := m.ID.MarshalBinary()
@@ -47,7 +107,7 @@ func (ss *MessageStore) AddMessage(m pigeon.Message) error {
 func (ss *MessageStore) GetMessage(id ulid.ULID) (*pigeon.Message, error) {
 	var msg pb.Message
 
-	err := ss.DB.View(func(tx *bolt.Tx) error {
+	err := ss.Dst.DB.View(func(tx *bolt.Tx) error {
 		b := tx.Bucket(msgBucket)
 		k, err := id.MarshalBinary()
 		if err != nil {
@@ -76,7 +136,7 @@ func (ss *MessageStore) GetMessage(id ulid.ULID) (*pigeon.Message, error) {
 func (ss *MessageStore) UpdateContent(id ulid.ULID, content []byte) error {
 	var msg pb.Message
 
-	err := ss.DB.Update(func(tx *bolt.Tx) error {
+	err := ss.Dst.DB.Update(func(tx *bolt.Tx) error {
 		b := tx.Bucket(msgBucket)
 		k, err := id.MarshalBinary()
 		if err != nil {
@@ -104,7 +164,7 @@ func (ss *MessageStore) UpdateContent(id ulid.ULID, content []byte) error {
 func (ss *MessageStore) UpdateStatus(id ulid.ULID, status pigeon.MessageStatus) error {
 	var msg pb.Message
 
-	err := ss.DB.Update(func(tx *bolt.Tx) error {
+	err := ss.Dst.DB.Update(func(tx *bolt.Tx) error {
 		b := tx.Bucket(msgBucket)
 		k, err := id.MarshalBinary()
 		if err != nil {
